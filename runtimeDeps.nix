783be796e2e6185cd490b51f66799c61af5a1762
Jesper Vesterberg <jumziey@gmail.com>
2024-03-16 21:30:15 +0100

    fix(clipboard): xclip dep

--- runtimeDeps.nix
+++ runtimeDeps.nix
@@ -1,6 +1,7 @@
 { pkgs }:
 with pkgs; [
   lazygit
+  xclip
   # packages with results in /lib/node_modules/.bin must come at the end
   nodePackages.typescript
   nodePackages.typescript-language-server
