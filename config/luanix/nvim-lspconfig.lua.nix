fa4634c21c3c0f3276a42f855ca22c2363475339
Jesper Vesterberg <jumziey@gmail.com>
2024-03-18 08:08:54 +0100

    add(plugin): helm lsp configuration

--- config/luanix/nvim-lspconfig.lua.nix
+++ config/luanix/nvim-lspconfig.lua.nix
@@ -1,6 +1,25 @@
 # vim: ft=lua
 { pkgs }:
 ''
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<C-i>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<C-g>', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-w>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<C-a>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+end
+
+require'lspconfig'.helm_ls.setup{
+  on_attach = on_attach,
+}
+
 local nvim_lsp = require("lspconfig")
 nvim_lsp.tsserver.setup({
   init_options = {
--- runtimeDeps.nix
+++ runtimeDeps.nix
@@ -3,6 +3,7 @@ with pkgs; [
   lazygit
   xclip
   fzf
+  helm-ls
   # packages with results in /lib/node_modules/.bin must come at the end
   nodePackages.typescript
   nodePackages.typescript-language-server
